aafed4ff6b34ebee4b79548029a562c0902fbf19
Sourabh124421 <128833504+Sourabh124421@users.noreply.github.com>
2023-04-18 22:04:10 +0530

    modified return statements and updated .py file

--- csvprocess.py
+++ csvprocess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[34]:
+# In[ ]:
 
 
 import csv
@@ -15,7 +15,7 @@ def read_csv_file():
             return data, file_path
     except:
         print(f"Error: Could not read file: {file_path}")
-        return [], file_path
+    return [], file_path
 
 def retrieve_host_data(file_path):
     if not file_path:
@@ -40,14 +40,15 @@ def retrieve_host_data(file_path):
         created_date = host_data['host_since']
         # Print all the data about the host ID
         print(f"Name of listing: {name}\nHost name: {host_name}\nDescription: {description}\nHost location: {host_location}\nCreated date: {created_date}")
+        return[host_data]
     else:
         # Print message to be displayed if hostID is not found 
         print(f"Sorry! No Data found regarding HostID:{host_id}")
+    return "NO DATA"
 
 def filter_by_location(file_path):
     # Take input from user for location
     location = input("What location are you looking for?: ")
-
     # Open the CSV file
     with open(file_path, encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)
@@ -55,17 +56,18 @@ def filter_by_location(file_path):
         for row in reader:
             if row['host_location'] == location:
                 filtered_data.append({'host_name': row['host_name'], 'property_type': row['property_type'], 'price': row['price'], 'minimum_nights': row['minimum_nights'], 'maximum_nights': row['maximum_nights']})
-        if not any(row['host_location'] == location for row in reader):
+        if filtered_data:
+            print("Here is the data you requested for -")
+            for row in filtered_data:
+                print(row) 
+            return "HOPE YOU'RE HAPPY WITH THE DATA"
+        else:
             print(f"Sorry I was unable to find anything related to '{location}'")
-            return
-    # Print the filtered data
-    print("Here is the data you requested for -")
-    for row in filtered_data:
-        print(row)
+    return "NO DATA"
 
 def filter_by_property_type(file_path):
     # Take input from user for property type
-    property_type = input('What Kind Of Property Are You Looking For ? -\n -renatl unit \n -guesthouse \n -houseboat \n -townhouse \n -home \n -condo \n -loft \n -cottage \n -guest suite \n -boat \n -cabin \n -serviced apartment \n -bed and breakfast \n -Yurt \n -villa \n -farm stay \n -penthouse \n -casa particular \n -apartment \n -studio \n -hut \n -bungalow \n -treehouse \n')
+    property_type = input('What Kind Of Property Are You Looking For ? -\n -rental unit \n -guesthouse \n -houseboat \n -townhouse \n -home \n -condo \n -loft \n -cottage \n -guest suite \n -boat \n -cabin \n -serviced apartment \n -bed and breakfast \n -Yurt \n -villa \n -farm stay \n -penthouse \n -casa particular \n -apartment \n -studio \n -hut \n -bungalow \n -treehouse \n')
     # Open the CSV file
     with open(file_path, encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)
@@ -73,36 +75,36 @@ def filter_by_property_type(file_path):
         for row in reader:
             if row['property_type'] == property_type:
                 filtered_data.append({'room_type': row['room_type'], 'accommodates': row['accommodates'], 'bathrooms': row['bathrooms_text'], 'bedrooms': row['bedrooms'], 'beds': row['beds']})
-        if not any(row['property_type'] == property_type for row in reader):
-            print(f"OPPS ! There seems to be no data available related to '{property_type}'")
-            return
-    # Print the filtered data
-    print("Here is the data you requested for -")
-    for row in filtered_data:
-        print(row)
-
+        if filtered_data:
+            print("Here is the data you requested for -")
+            for row in filtered_data:
+                print(row)
+            return "HOPE YOU'RE HAPPY WITH THE DATA"
+        else:
+            print(f"Sorry, there seems to be no data available related to '{property_type}'")
+    return "NO DATA"
+        
+        
 def filter_by_host_location(file_path):
     location = input('Please provide a location:')
     host_name = input('Please Input a Name:')
-    try:
-        with open(file_path, encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            rows = list(reader)
+    with open(file_path, encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        rows = list(reader)
         filtered_data = []
         for row in rows:
             if row['host_location'] == location and row['host_name'] == host_name:
                 filtered_data.append({'acceptance_rate': row['host_acceptance_rate'], 'amenities': row['amenities'], 'instant_bookable': row['instant_bookable']})
-        if not filtered_data:
-            print(f"Sorry ! No information related to '{location}' and '{host_name}'")
-            return []
-        else:
+        if filtered_data:
             print("HERE IS THE DATA YOU REQUESTED FOR:")
             for row in filtered_data:
                 print(row)
-    except:
-        print(f"Error: Could not open file '{file_path}'.")
-        return []
+            return "HOPE YOU'RE HAPPY WITH THE DATA"
+        else:
+            print(f"Sorry !!! No information related to '{location}' and '{host_name}'")
+    return "NO DATA"
 
+        
 # When i created it i wanted to test it and hence i called CSV file data and file path using read_csv_file() function and now im leaving it as commented
 data, file_path = read_csv_file()
 
